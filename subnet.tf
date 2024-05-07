54e3b217f671152611980383db1430134ee538e3
ShrideviRShetty <158450109+ShrideviRShetty@users.noreply.github.com>
2024-05-07 22:54:33 +0530

    Create subnet.tf

---
+++ subnet.tf
@@ -0,0 +1,11 @@
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+}
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-south-1b"
+}
