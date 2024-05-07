c00bbb057b8b73242e9f053c2cbba7c447d25eb5
ShrideviRShetty <158450109+ShrideviRShetty@users.noreply.github.com>
2024-05-07 22:49:54 +0530

    Create gateway.tf

---
+++ gateway.tf
@@ -0,0 +1,12 @@
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public.id
+}
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
