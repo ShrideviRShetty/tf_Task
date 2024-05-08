7f7846e6840abd9aabbae64983376e91921fc6ff
ShrideviRShetty <158450109+ShrideviRShetty@users.noreply.github.com>
2024-05-08 11:27:34 +0530

    Update app_ec2.tf

--- app_ec2.tf
+++ app_ec2.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "web" {
   instance_type   = "t2.micro"
   subnet_id       = aws_subnet.public.id
   key_name        = "simple"
+  associate_public_ip_address = true
   security_groups = [aws_security_group.web.id]
   user_data = file("nginx.sh")
   tags = {
