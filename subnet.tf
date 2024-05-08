1063b44b0747464f40a9294154d6a3b9c10529ac
ShrideviRShetty <158450109+ShrideviRShetty@users.noreply.github.com>
2024-05-08 11:25:24 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -2,6 +2,7 @@ resource "aws_subnet" "public" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "ap-south-1a"
+  map_public_ip_on_launch = true
   tags = {
   Name = "publicSubnet"
  }
@@ -11,6 +12,7 @@ resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "ap-south-1b"
+  map_public_ip_on_launch = true
   tags = {
   Name = "pvtSubnet"
  }
