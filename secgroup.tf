8afe41a1ef7b0a228dc5a71471ac208f1312b55e
ShrideviRShetty <158450109+ShrideviRShetty@users.noreply.github.com>
2024-05-10 14:33:59 +0530

    Update secgroup.tf

--- secgroup.tf
+++ secgroup.tf
@@ -4,16 +4,9 @@ resource "aws_security_group" "web" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port   = 00
+    to_port     = 00
+    protocol    = "all"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -31,9 +24,9 @@ resource "aws_security_group" "db" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "all"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
