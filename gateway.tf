bbc21f4c66bf2059a35281336dd30b4dc2c048df
ShrideviRShetty <158450109+ShrideviRShetty@users.noreply.github.com>
2024-05-10 12:39:22 +0530

    Update gateway.tf

--- gateway.tf
+++ gateway.tf
@@ -1,10 +1,16 @@
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "my_igw"
+  }
 }
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public.id
+  tags = {
+    Name = "my_nat_gateway"
+  }
 }
 
 resource "aws_eip" "nat" {
